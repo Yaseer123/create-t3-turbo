008c197fbe2083fc1696bc8c6234260f290cdd2a
Yaseer123 <yaseerarafat230@gmail.com>
2024-09-19 13:23:12 +0600

    first

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -19,7 +19,6 @@ export default function HomePage() {
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            <div>gg</div>
             Create <span className="text-primary">T3</span> Turbo
           </h1>
           <AuthShowcase />
